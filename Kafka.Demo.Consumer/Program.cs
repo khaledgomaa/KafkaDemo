7a7bdd4e74fb647c796eafee039b7c598e782423
KhaledGomaa <khaledgomaa670@gmail.com>
2024-12-16 20:50:04 +0200

    Added producer and consumer

---
+++ Kafka.Demo.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+
+var kafkaBootstrapServer = "localhost:9092";
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = kafkaBootstrapServer,
+    GroupId = "InventoryConsumerGroup",
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+};
+
+IConsumer<Ignore, string> _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+
+Console.WriteLine("Enter the topic name: ");
+var topic = Console.ReadLine();
+
+_consumer.Subscribe(topic);
+
+while (true)
+{
+    var consumeResult = _consumer.Consume();
+
+    if (consumeResult.Message != null)
+    {
+        var message = consumeResult.Message.Value;
+
+        Console.WriteLine("A new message is consumed: {0}", message);
+    }
+}
---
+++ Kafka.Demo.Producer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+
+var kafkaBootstrapServer = "localhost:9092";
+
+var producerconfig = new ProducerConfig
+{
+    BootstrapServers = kafkaBootstrapServer
+};
+
+IProducer<Null, string> _producer = new ProducerBuilder<Null, string>(producerconfig).Build();
+
+Console.WriteLine("Enter the topic name: ");
+var topic = Console.ReadLine();
+
+string? message = string.Empty;
+
+do
+{
+    Console.WriteLine("Enter a message to procude: ");
+    message = Console.ReadLine();
+
+    if (!string.IsNullOrWhiteSpace(message))
+    {
+        var kafkamessage = new Message<Null, string> { Value = message };
+
+        await _producer.ProduceAsync(topic, kafkamessage);
+    }
+} while (!string.IsNullOrWhiteSpace(message));
